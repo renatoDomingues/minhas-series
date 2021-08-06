8e7f657906cf7c0364f14c0de2efbee25c06605b
renatoDomingues <kungfurenato30@gmail.com>
2021-08-06 14:45:34 -0300

    06/08/2021

--- index.js
+++ index.js
@@ -5,20 +5,24 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 3000
 
-const mongo = process.env.MONGODB || 'mongodb://localhost/minhas-series'
+//const mongo = process.env.MONGODB || 'mongodb://localhost/minhas-series'
+const mongo = process.env.MONGODB || 'mongodb+srv://minhas-series:minhas-series@cluster0.opt5r.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
 const mongoose = require('mongoose')
-mongoose.Promesa = global.Promise
+mongoose.Promise = global.Promise
 
-app.get('/', (req, res)=> res.send('ok'))
+
+app.get('/', (req, res) => res.send('ok'))
+
 
 mongoose
-    .connect(mongo, {useNewUrlParser: true, useUnifiedTopology: true})
-    .then(()=>{
-        app.listen(port, ()=>
+    .connect(mongo, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then( () => {
+        app.listen(port, () =>
             console.log('Listening on: '+port))
     })
-    .catch(e=>{
+    .catch( e => {
         console.log(e)
     })
 
-//app.listen(port, ()=>console.log('Listening on: '+port))
+//app.listen(port, () => console.log('Listening on: '+port)) ]
+//mongodb => 'mongodb+srv://minhas-series:minhas-series@cluster0.opt5r.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
--- readme.md
+++ readme.md
@@ -1,12 +1,16 @@
 
 ## SOBRE
 ````
+Manuseio banco de dados com mongodb, npm, express mongoose, body-parser e ejs.
+
+Database handling with mongodb, npm, express mongoose, body-parser and ejs.
 ````
 
 ### Install
 ````
 * npm init
 * yarn add express mongoose body-parser
+* yarn add ejs
 ````
 
 ### Terminal
